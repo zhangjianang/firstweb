0e54406bf2ff9477607c6c64c9955689f6f86f03
zhangjianang <1364487228@qq.com>
2019-08-19 19:03:24 +0800

    rocketmq

--- src/main/java/com/ang/firstweb/myclassloader/GCTry.java
+++ src/main/java/com/ang/firstweb/myclassloader/GCTry.java
@@ -4,15 +4,13 @@ package com.ang.firstweb.myclassloader;
  * Created by ang on 2019/8/18.
  */
 public class GCTry {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         byte[] allocation,allocation2,allocation3;
         allocation = new byte[28000*1024];
         allocation2 = new byte[28000*1024];
         allocation3   = new byte[28000*1024];
-        while (true){
 
-
-        }
+        Thread.sleep(Integer.MAX_VALUE);
 
     }
 }
