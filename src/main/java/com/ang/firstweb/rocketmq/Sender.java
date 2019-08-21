2c42c6a32bb281b596b1cb391a7feeb7d5d7a399
zhangjianang <1364487228@qq.com>
2019-08-21 10:04:37 +0800

    生产消费实例

--- src/main/java/com/ang/firstweb/rocketmq/Sender.java
+++ src/main/java/com/ang/firstweb/rocketmq/Sender.java
@@ -1,18 +1,62 @@
 package com.ang.firstweb.rocketmq;
 
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by adimn on 2019/8/19.
  */
 public class Sender {
+//    SyncProducer
     public static void main(String[] args) throws Exception {
+        AsyncSender();
+    }
+
+    public static void AsyncSender() throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException {
+            //Instantiate with a producer group name.
+            DefaultMQProducer producer = new DefaultMQProducer("group_a");
+            // Specify name server addresses.
+            producer.setNamesrvAddr("localhost:9876");
+            //Launch the instance.
+            producer.start();
+            producer.setRetryTimesWhenSendAsyncFailed(0);
+            for (int i = 0; i < 30; i++) {
+                final int index = i;
+                //Create a message instance, specifying topic, tag and message body.
+                Message msg = new Message("topic-a",
+                        "tag-a",
+                        "OrderID"+i,
+                        "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+                producer.send(msg, new SendCallback() {
+                    @Override
+                    public void onSuccess(SendResult sendResult) {
+                        System.out.printf("%-10d OK %s %n", index,
+                                sendResult.getMsgId());
+                    }
+                    @Override
+                    public void onException(Throwable e) {
+                        System.out.printf("%-10d Exception %s %n", index, e);
+                        e.printStackTrace();
+                    }
+                });
+            }
+            //Shut down once the producer instance is not longer in use.
+//            producer.shutdown();
+    }
+
+
+    public static void syncSender() throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException, MQBrokerException {
         //Instantiate with a producer group name.
         DefaultMQProducer producer = new
-                DefaultMQProducer("please_rename_unique_group_name");
+                DefaultMQProducer("ang_first");
         // Specify name server addresses.
         producer.setNamesrvAddr("localhost:9876");
         //Launch the instance.
@@ -21,8 +65,7 @@ public class Sender {
             //Create a message instance, specifying topic, tag and message body.
             Message msg = new Message("TopicTest" /* Topic */,
                     "TagA" /* Tag */,
-                    ("Hello RocketMQ " +
-                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
             );
             //Call send message to deliver message to one of brokers.
             SendResult sendResult = producer.send(msg);
@@ -30,5 +73,6 @@ public class Sender {
         }
         //Shut down once the producer instance is not longer in use.
         producer.shutdown();
+
     }
 }
