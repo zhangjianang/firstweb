b050cd886daec1cddbf454a01b8edbc0dc48f845
zhangjianang <1364487228@qq.com>
2019-11-25 19:06:05 +0800

    leetcode 动态规划更多

--- src/main/java/com/ang/firstweb/lettcode/ClimbStairs70.java
+++ src/main/java/com/ang/firstweb/lettcode/ClimbStairs70.java
@@ -7,48 +7,121 @@ import java.util.Map;
  * Created by adimn on 2019/11/22.
  */
 public class ClimbStairs70 {
-    private static Map<Integer,Integer> res = new HashMap<>();
+    private static Map<Integer, Integer> res = new HashMap<>();
+
     public static int climbStairs(int n) {
-        if(n<=1){
-            res.put(n,1);
+        if (n <= 1) {
+            res.put(n, 1);
             return 1;
         }
-        if(res.containsKey(n)){
+        if (res.containsKey(n)) {
             return res.get(n);
         }
-        res.put(n,climbStairs(n-2)+climbStairs(n-1));
+        res.put(n, climbStairs(n - 2) + climbStairs(n - 1));
         return res.get(n);
     }
 
-    public int climbStairs2(int n){
+    public int climbStairs2(int n) {
         HashMap<Integer, Integer> data = new HashMap<>();
-        return withnum(n,data);
+        return withnum(n, data);
     }
 
-    public int withnum(int n,Map<Integer,Integer> cur){
-        if(n<=1){
-            cur.put(n,1);
+    public int withnum(int n, Map<Integer, Integer> cur) {
+        if (n <= 1) {
+            cur.put(n, 1);
             return 1;
         }
-        if(cur.containsKey(n)){
+        if (cur.containsKey(n)) {
             return cur.get(n);
         }
-        cur.put(n,withnum(n-2,cur)+withnum(n-1,cur));
+        cur.put(n, withnum(n - 2, cur) + withnum(n - 1, cur));
         return cur.get(n);
     }
 
 
     public int dyna(int n) {
-        int[] dp = new int[n+1];
-        dp[0] =1;
-        dp[1] =1;
-        if(n<2){
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        if (n < 2) {
             return dp[n];
         }
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1]+dp[i-2];
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+    }
+
+
+    public int rob(int[] nums) {
+        if (nums.length < 1) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] val = new int[nums.length];
+        val[0] = nums[0];
+        val[1] = Math.max(nums[1], nums[0]);
+        int max = Math.max(val[0], val[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            val[i] = Math.max(val[i - 1], val[i - 2] + nums[i]);
+            max = Math.max(val[i], max);
+        }
+        return max;
+    }
+
+    public int maxProduct(int[] nums) {
+        int[] pos = new int[nums.length];
+        int[] neg = new int[nums.length];
+        pos[0] = nums[0];
+        neg[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            pos[i] = Math.max(nums[i], Math.max(pos[i - 1] * nums[i], neg[i - 1] * nums[i]));
+            neg[i] = Math.min(nums[i], Math.min(pos[i - 1] * nums[i], neg[i - 1] * nums[i]));
+            max = Math.max(max, pos[i]);
+        }
+        return max;
+    }
+
+    public int maxSubArray(int[] nums) {
+        int[] pp = new int[nums.length];
+        pp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                if (pp[i - 1] < 0) {
+                    pp[i] = nums[i];
+                } else {
+                    pp[i] = pp[i - 1] + nums[i];
+                }
+            } else {
+                if (pp[i - 1] < 0) {
+                    pp[i] = nums[i];
+                } else {
+                    pp[i] = pp[i - 1] + nums[i];
+                }
+            }
+            max = Math.max(pp[i], max);
+        }
+        return max;
+    }
+
+
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length()][p.length()];
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i][j] = true;
+                }
+
+            }
+        }
+        return true;
     }
 
 
--- src/test/java/com/ang/firstweb/lettcode/ClimbStairsTest.java
+++ src/test/java/com/ang/firstweb/lettcode/ClimbStairsTest.java
@@ -9,26 +9,28 @@ import static org.junit.Assert.*;
  * Created by adimn on 2019/11/22.
  */
 public class ClimbStairsTest {
-    ClimbStairs70 cc ;
+    ClimbStairs70 cc;
+
     @Before
-    public void init(){
+    public void init() {
         cc = new ClimbStairs70();
     }
+
     @Test
     public void climbStairs() throws Exception {
-        assertEquals(2,ClimbStairs70.climbStairs(2));
+        assertEquals(2, ClimbStairs70.climbStairs(2));
         assertEquals(2, cc.climbStairs2(2));
     }
 
     @Test
     public void climbStairs1() throws Exception {
-        assertEquals(21,ClimbStairs70.climbStairs(7));
+        assertEquals(21, ClimbStairs70.climbStairs(7));
         assertEquals(21, cc.climbStairs2(7));
     }
 
     @Test
     public void climbStairs2() throws Exception {
-        assertEquals(13,ClimbStairs70.climbStairs(6));
+        assertEquals(13, ClimbStairs70.climbStairs(6));
         assertEquals(13, cc.climbStairs2(6));
     }
 
@@ -43,4 +45,37 @@ public class ClimbStairsTest {
         assertEquals(39088169, cc.dyna(37));
     }
 
+
+    @Test
+    public void robtest() {
+        int[] nums = {1, 2, 3, 1};
+        assertEquals(4, cc.rob(nums));
+    }
+
+    @Test
+    public void maxSubArray() {
+        int[] nums = {1, 2, 3, 1};
+        int[] nums1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        assertEquals(7, cc.maxSubArray(nums));
+        assertEquals(6, cc.maxSubArray(nums1));
+    }
+
+    @Test
+    public void isMatch() {
+        String str = "aa";
+        String p = "a";
+        assertEquals(true, cc.isMatch(str, p));
+        String str1 = "aa";
+        String p1 = "a*";
+        assertEquals(true, cc.isMatch(str1, p1));
+        String str2 = "ab";
+        String p2 = ".*";
+        assertEquals(true, cc.isMatch(str2, p2));
+        String str3 = "aab";
+        String p3 = "c*a*b";
+        assertEquals(true, cc.isMatch(str3, p3));
+        String str4 = "mississippi";
+        String p4 = "mis*is*p*.";
+        assertEquals(true, cc.isMatch(str4, p4));
+    }
 }
