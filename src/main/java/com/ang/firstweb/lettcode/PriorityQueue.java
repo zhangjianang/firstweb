2eee10fed423d91c53f7cb090bdbefee148700be
zhangjianang <1364487228@qq.com>
2019-12-09 18:49:30 +0800

    数据流中第k大

--- src/main/java/com/ang/firstweb/lettcode/PriorityQueue.java
+++ src/main/java/com/ang/firstweb/lettcode/PriorityQueue.java
@@ -17,7 +17,7 @@ public class PriorityQueue {
 
 
         for (int i = keys.length / 2; i >= 0; i--) {
-            rebalance(keys, i, keys.length,frequent);
+            rebalance(keys, i, keys.length, frequent);
         }
 
         List<Integer> res = new ArrayList<>();
@@ -25,7 +25,7 @@ public class PriorityQueue {
         for (int j = 0; j < k; j++) {
             res.add(Integer.parseInt(keys[0].toString()));
             numswitch(keys, 0, len - j);
-            rebalance(keys, 0, len - j,frequent);
+            rebalance(keys, 0, len - j, frequent);
         }
         return res;
     }
@@ -63,14 +63,49 @@ public class PriorityQueue {
         Integer[] num = new Integer[]{3, 6, 1, 7, 4, 9, 3};
         PriorityQueue pq = new PriorityQueue();
         for (int i = num.length / 2; i >= 0; i--) {
-            pq.rebalance(num, i, num.length,null);
+            pq.rebalance(num, i, num.length, null);
         }
 
         int len = num.length - 1;
         for (int j = 0; j < 3; j++) {
             pq.numswitch(num, 0, len - j);
-            pq.rebalance(num, 0, len - j,null);
+            pq.rebalance(num, 0, len - j, null);
         }
         Arrays.stream(num).forEach(System.out::println);
+    }
+}
+
+class KthLargest {
+    private java.util.PriorityQueue<Integer> pq = new java.util.PriorityQueue();
+    public KthLargest(int k, int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(nums[i]);
+            if (pq.size()>k) {
+                pq.poll();
+            }
+        }
+    }
+    public int add(int val) {
+        if(val>pq.peek()) {
+            pq.add(val);
+            pq.poll();
+        }
+        return  pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,8,2};
+        KthLargest kthLargest = new KthLargest(3, arr);
+
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10)); // returns 5
+        System.out.println(kthLargest.add(9));
+        // returns 8
+        System.out.println(kthLargest.add(4));
+        // returns 8
+
+//["KthLargest","add","add","add","add","add"]
+//[[1,[]],[-3],[-2],[-4],[0],[4]]
     }
 }
